bfbb3ece696e7f64e4fa32b7bc9a293ac20c4dd1
Jason Pelzer <github.com@pelzer.com>
2021-04-19 18:27:02 -0400

    Changed to parse Govee H5075

--- src/govee-data.cpp
+++ src/govee-data.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 void GoveeData::decodeData(const char* const data)
 {
-	int temp = ((signed char)(data[4]) << 8) | int(data[3]);
-  humidity = float((int(data[6]) << 8 | int(data[5])))/100;
-  battery = int(data[7]);
+	int iTemp = int(data[3]) << 16 | int(data[4]) << 8 | int(data[5]);
+	temperatureC = float(iTemp) / 10000.0;
+	humidity = float(iTemp % 1000) / 10.0;
+	battery = int(data[6]);
+
 	ma = int(data[8]);
-	temperatureC = float(temp)/100;
-  temperatureF = temperatureC*9/5+32;
+	temperatureF = temperatureC*9/5+32;
 }
 
 void GoveeData::calcAverage(std::queue<GoveeData>* it)
